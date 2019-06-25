976acb7c1cb14f0329b448767023548acde0e649
Olatunbosun Adeoye <adex001@gmail.com>
2019-06-25 19:57:40 +0100

    added dotenv to database configurations

--- server/controller/user.js
+++ server/controller/user.js
@@ -5,34 +5,42 @@ import Token from '../helpers/token';
 
 class UserController {
   static async signup(req, res) {
-    const {
-      email, password, firstname, lastname
-    } = req.body;
-    const encryptedPassword = await HashPassword.create(password);
-    const userObject = {
-      email,
-      firstname,
-      password: encryptedPassword,
-      lastname,
-    };
-    const data = await userModel.findUser(email);
-    if (data) return Response.error(res, 400, 'Email exists already! try another!');
-    const payload = await userModel.create(userObject);
-    if (payload) {
-      const payloadObject = {
-        userId: payload.userid,
-        email: payload.email,
+    try {
+      const {
+        email, password, firstname, lastname
+      } = req.body;
+      const encryptedPassword = await HashPassword.create(password);
+      const userObject = {
+        email,
+        firstname,
+        password: encryptedPassword,
+        lastname,
       };
-      const token = await Token.createToken(payloadObject);
-      payloadObject.firstname = payload.firstname;
-      payloadObject.lastname = payload.lastname;
-
-      return Response.success(res, 201, {
-        user: payloadObject,
-        token,
-      });
+      const data = await userModel.findUser(email);
+      console.log('------------data----------')
+      console.log(data);
+      if (data) return Response.error(res, 400, 'Email exists already! try another!');
+      const payload = await userModel.create(userObject);
+      if (payload) {
+        const payloadObject = {
+          userId: payload.userid,
+          email: payload.email,
+        };
+        const token = await Token.createToken(payloadObject);
+        payloadObject.firstname = payload.firstname;
+        payloadObject.lastname = payload.lastname;
+  
+        return Response.success(res, 201, {
+          user: payloadObject,
+          token,
+        });
+      }
+    } catch (err) {
+      console.log('--------------error-----------------');
+      
+      console.log(err);
+      return Response.error(res, 500, 'Internal server error!');
     }
-    return Response.error(res, 500, 'Internal server error!');
   }
 }
 
