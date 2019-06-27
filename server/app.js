bbbdf1863cae0473bdd375851fb32876f41a014d
Olatunbosun Adeoye <adex001@gmail.com>
2019-06-27 17:29:27 +0100

    implemented update recipe

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser';
 
 
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
